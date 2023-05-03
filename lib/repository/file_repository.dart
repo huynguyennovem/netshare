ae78e96a431cbc5cbe507aa920b66f053388a480
huynguyennovem <huynguyennovem@gmail.com>
2023-05-03 19:40:49 +0700

    Fixes #47: URL is still cached with old address
    Signed-off-by: huynguyennovem <huynguyennovem@gmail.com>

--- lib/repository/file_repository.dart
+++ lib/repository/file_repository.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:dartz/dartz.dart';
 import 'package:netshare/data/api_service.dart';
+import 'package:netshare/data/global_scope_data.dart';
 import 'package:netshare/data/hivedb/clients/shared_file_client.dart';
 import 'package:netshare/di/di.dart';
 import 'package:netshare/entity/api_error.dart';
@@ -12,23 +13,32 @@ class FileRepository {
 
   FileRepository(this.apiService);
 
-  // Update file state if it's downloaded before/existing on device storage
+  // 1. Fetch list file from remote server
+  // 2. Update file state if it's downloaded before or existing on device storage
   Future<Either<ApiError, Set<SharedFile>>> getSharedFilesWithState() async {
     final originalFiles = await apiService.getSharedFiles();
     if (originalFiles.isLeft()) {
       return Left(originalFiles.fold((l) => l, (r) => throw UnimplementedError()));
     }
     final originalFilesRight = originalFiles.getOrElse(() => {});
+
+    // For the context why needs to update file's URL, see:
+    // https://github.com/huynguyennovem/netshare/issues/47
+    String connectedAddress = 'http://${getIt.get<GlobalScopeData>().connectedIPAddress}';
+
     final newFileList = await Future.wait(originalFilesRight.map((file) async {
       // check exist in Hive
       final savedAvailableFile = await getIt.get<SharedFileClient>().get(file.name);
-      if (null == savedAvailableFile) return file;
+      if (null == savedAvailableFile) {
+        return file.copyWith(url: '$connectedAddress/${file.name}');
+      }
 
       // check exist in device storage
       final savedFile = File('${savedAvailableFile.savedDir}/${savedAvailableFile.name!}');
       final isFileExisting = await savedFile.exists();
-
-      return isFileExisting ? savedAvailableFile : file;
+      return isFileExisting
+          ? savedAvailableFile.copyWith(url: '$connectedAddress/${file.name}')
+          : file.copyWith(url: '$connectedAddress/${file.name}');
     }));
     return Right(newFileList.toSet());
   }
